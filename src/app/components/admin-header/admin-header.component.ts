8d2eb9afa4164013b1142ff8e18b27378ce9abf1
The D Code <thedcode86@gmail.com>
2019-09-29 14:14:02 -0400

    Adding admin-header component

---
+++ src/app/components/admin-header/admin-header.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'admin-header',
+  templateUrl: './admin-header.component.html',
+  styleUrls: ['./admin-header.component.css']
+})
+export class AdminHeaderComponent {
+  TITLE = 'FFQR Admin Portal';
+}
