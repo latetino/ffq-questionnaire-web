9fe0e8f856ca01e175aee83c99651646b765fb84
The D Code <thedcode86@gmail.com>
2019-10-01 21:59:10 -0400

    Adding ffqsugar class

---
+++ src/app/models/ffqsugar.ts
@@ -0,0 +1,15 @@
+import {FFQItemInput} from './ffqitem-input';
+import {FFQItemResponse, FoodType} from './ffqitem-response';
+import {Serving} from './ffqitem-response';
+
+export class FFQSugar {
+  additionalSugar: boolean;
+  teaspoons: number;
+  
+  constructor(additionalSugar: boolean, teaspoons: number) {
+    this.additionalSugar = additionalSugar;
+    this.teaspoons = teaspoons;
+  }
+
+ 
+}
