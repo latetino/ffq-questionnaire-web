e65d327ec9c4a73fea39438814b9ec6706a40e86
The D Code <thedcode86@gmail.com>
2019-10-01 22:00:08 -0400

    Adding wiring in add food-nutrients page

--- src/app/pages/fooditem/fooditem.component.ts
+++ src/app/pages/fooditem/fooditem.component.ts
@@ -1,16 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {MatDialog} from '@angular/material';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {FoodItemService} from '../../services/food-item/food-item.service';
-import { FFQItem } from 'src/app/models/ffqitem';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FoodItemService } from '../../services/food-item/food-item.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { ErrorDialogPopupComponent } from 'src/app/components/error-dialog-popup/error-dialog-popup.component';
 import { FFQFoodNutrientsResponse } from 'src/app/models/ffqfoodnutrients-response';
-import { FormGroup, FormControl, Validators,  ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Subscription } from 'rxjs/internal/Subscription';
-
-
+import { FFQFoodNutrients } from 'src/app/models/ffqfoodnutrients';
 
 @Component({
   selector: 'app-fooditem',
@@ -20,14 +18,10 @@ import { Subscription } from 'rxjs/internal/Subscription';
 export class FooditemComponent implements OnInit {
 
   TITLE = 'FFQR Food Item Portal';
-  fooditemform: FormGroup;
-  nutrientsform: FormGroup;
-  validMessage: string ="";
   private routeSub: Subscription;
-  
-  
-  
-  constructor(public foodService: FoodItemService,
+
+  constructor(
+    public foodService: FoodItemService,
     private activatedRoute: ActivatedRoute,
     private errorDialog: MatDialog,
     private submissionErrorDialog: MatDialog,
@@ -35,75 +29,26 @@ export class FooditemComponent implements OnInit {
     private successDialog: MatDialog,
     private router: Router,
     private modalService: NgbModal,
-    private route: ActivatedRoute) {}
+    private route: ActivatedRoute) { }
 
-
-foodNutrients: FFQFoodNutrientsResponse[] = [];
-foodNutrientsItem: FFQFoodNutrientsResponse[] = [];
-dataLoaded: Promise<boolean>;
-
+  foodNutrientsItem: FFQFoodNutrientsResponse[] = [];
+  dataLoaded: Promise<boolean>;
 
   ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id');
-    console.log(id);
-    this.getFoodByName(id);
 
-    this.loadFoodsAndNutrients();
-    console.log( this.foodNutrients);
-    this.fooditemform =new FormGroup({
-    fooditemname: new FormControl ('', Validators.required),
-    sugaradded: new FormControl ('', Validators.required),
-    teaspoons: new FormControl ('', Validators.required),
-    servings: new FormControl ('', Validators.required),
-    primary: new FormControl ('', Validators.required),
-    });
-    this.nutrientsform =new FormGroup({
-    Retinol: new FormControl ('', Validators.required),
-    VitD: new FormControl ('', Validators.required),
-    VitE : new FormControl ('', Validators.required),
-    VitK: new FormControl ('', Validators.required),
-    VitC: new FormControl ('', Validators.required),
-    Thiamin: new FormControl ('', Validators.required),
-    Riboflavin : new FormControl ('', Validators.required),
-    Niacin: new FormControl ('', Validators.required),
-    PanthotenicAcid : new FormControl ('', Validators.required),
-    VitB6: new FormControl ('', Validators.required),
-    Folate : new FormControl ('', Validators.required),
-    VitB12: new FormControl ('', Validators.required),
-    Calcium: new FormControl ('', Validators.required),
-    Phosphorus: new FormControl ('', Validators.required),
-    Magnesium : new FormControl ('', Validators.required),
-    Iron: new FormControl ('', Validators.required),
-    Zinc : new FormControl ('', Validators.required),
-    Copper: new FormControl ('', Validators.required),
-    Potassium: new FormControl ('', Validators.required),
-    });
-  }
-  
-  private handleFoodServiceError(error: HttpErrorResponse) {
-    console.error('Error occurred.\n' + error.message);
-    const dialogRef = this.errorDialog.open(ErrorDialogPopupComponent);
-    dialogRef.componentInstance.title = 'Error Fetching Food Items';
-    dialogRef.componentInstance.message = error.message;
-    dialogRef.componentInstance.router = this.router;
-    dialogRef.afterClosed().subscribe(() => {
-      this.router.navigateByUrl('/');
-    });
+    const getFoodByObjectId = this.route.snapshot.paramMap.get('id');
+
+    this.getFoodByObjectId(getFoodByObjectId);
+
+    console.log(this.foodNutrientsItem);
   }
 
-  private loadFoodsAndNutrients() {
-    this.foodService.getAllFoods().subscribe(data => {
-      data.map(response => {        
-        this.foodNutrients.push(response);
-      });
-      console.log(this.foodNutrients.length + ' foods and its nutrients were returned from server.');
-      this.dataLoaded = Promise.resolve(true);
-    }, (error: HttpErrorResponse) => this.handleFoodServiceError(error));
-  }
-  private getFoodByName(name: string) {
-    this.foodService.getFoodbyName(name).subscribe(data => {this.foodNutrientsItem.push(data)});
+  private getFoodByObjectId(name: string) {
+    this.foodService.getFoodbyName(name).subscribe(data => {
+      this.foodNutrientsItem.push(FFQFoodNutrients.foodItemFromResponse(data))
+    });
     this.dataLoaded = Promise.resolve(true);
   }
-  
-
 }
+
+
