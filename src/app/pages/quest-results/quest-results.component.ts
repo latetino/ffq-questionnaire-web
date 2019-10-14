2d3c25fcc68bcf1924dc20015a8243b22e55ce97
Daykel Muro <Daykel_Muro@ryder.com>
2019-10-14 14:01:09 -0400

    if no questionnaire submitted show message

--- src/app/pages/quest-results/quest-results.component.ts
+++ src/app/pages/quest-results/quest-results.component.ts
@@ -4,25 +4,21 @@ import { FFQResultsResponse } from 'src/app/models/ffqresultsresponse';
 
 // Questionnaire reesults page added by Daykel Muro 09/30/2019
 @Component({
-  selector: 'app-quest-results',
-  templateUrl: './quest-results.component.html',
-  styleUrls: ['./quest-results.component.css']
+  selector: "app-quest-results",
+  templateUrl: "./quest-results.component.html",
+  styleUrls: ["./quest-results.component.css"]
 })
 export class QuestResultsComponent implements OnInit {
-
-  public show:boolean = false;
-  public buttonName:any = 'Show';
+  public show: boolean = false;
+  public buttonName: any = "Show";
+  MESSAGE = "No questionnaires have been submitted yet!";
 
   results: FFQResultsResponse[] = [];
 
-  constructor(public resultsService: ResultsService
-
-  ) { }
+  constructor(public resultsService: ResultsService) {}
 
   ngOnInit() {
     this.getAllResults();
-    console.log(this.results);
-
   }
 
   private getAllResults() {
@@ -30,7 +26,10 @@ export class QuestResultsComponent implements OnInit {
       data.map(response => {
         this.results.push(response);
       });
-      console.log(this.results.length + ' foods and its nutrients were returned from server.');
+      console.log(
+        this.results.length +
+          " foods and its nutrients were returned from server."
+      );
       //this.dataLoaded = Promise.resolve(true);
     });
   }
@@ -38,10 +37,7 @@ export class QuestResultsComponent implements OnInit {
     this.show = !this.show;
 
     // CHANGE THE NAME OF THE BUTTON.
-    if(this.show)  
-      this.buttonName = "Hide";
-    else
-      this.buttonName = "Show";
+    if (this.show) this.buttonName = "Hide";
+    else this.buttonName = "Show";
   }
-
 }
