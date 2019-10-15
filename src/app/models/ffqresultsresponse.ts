a138cf1eb907b27b9db66a6c660fc5790e5c25f4
The D Code <thedcode86@gmail.com>
2019-10-14 23:23:59 -0400

    reverting?

--- src/app/models/ffqresultsresponse.ts
+++ src/app/models/ffqresultsresponse.ts
@@ -1,30 +1,23 @@
-import { FFQFoodItem } from "./ffqfooditem";
-import { FFQNutrientlist } from "./ffqnutrientlist";
-import { FFQItemInput } from "./ffqitem-input";
-import { FoodType } from "./ffqitem-response";
+import { FFQFoodItem } from './ffqfooditem';
+import { FFQNutrientlist } from './ffqnutrientlist';
+import { FFQItemInput } from './ffqitem-input';
+import { FoodType } from './ffqitem-response';
 
 export class FFQResultsResponse {
-  questionnaireId: String;
-  patientName: string;
-  ageInMonths: number;
-  userChoices: any;
-  weeklyTotals: any;
-  dailyAverages: any;
-  show: boolean;
-
-  constructor(
-    id: string,
-    name: string,
-    age: number,
-    userChoices: any,
-    weeklyTotals: any,
-    dailyAverages: any
-  ) {
-    this.questionnaireId = id;
-    this.patientName = name;
-    this.ageInMonths = age;
-    this.userChoices = userChoices;
-    this.weeklyTotals = weeklyTotals;
-    this.dailyAverages = dailyAverages;
+    questionnaireId: String;
+    patientName: string;
+    ageInMonths: number;
+    userChoices: any;
+    weeklyTotals: any;  
+    dailyAverages: any;
+  
+    constructor(id: string, name: string, age: number, userChoices:any, weeklyTotals: any, dailyAverages: any ) {
+      this.questionnaireId = id;
+      this.patientName = name;
+      this.ageInMonths = age;
+      this.userChoices = userChoices;
+      this.weeklyTotals = weeklyTotals;
+      this.dailyAverages = dailyAverages;
+    }    
   }
-}
+  
