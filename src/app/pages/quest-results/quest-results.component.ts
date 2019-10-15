c6c408170c35e806e97ae7c78da55a31b297d6e6
The D Code <thedcode86@gmail.com>
2019-10-14 23:48:31 -0400

    Adding daykel's changes

--- src/app/pages/quest-results/quest-results.component.ts
+++ src/app/pages/quest-results/quest-results.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ResultsService } from 'src/app/services/results/results';
-import { FFQResultsResponse } from 'src/app/models/ffqresultsresponse';
+import { Component, OnInit } from "@angular/core";
+import { ResultsService } from "src/app/services/results/results";
+import { FFQResultsResponse } from "src/app/models/ffqresultsresponse";
 
 // Questionnaire reesults page added by Daykel Muro 09/30/2019
 @Component({
@@ -9,6 +9,10 @@ import { FFQResultsResponse } from 'src/app/models/ffqresultsresponse';
   styleUrls: ['./quest-results.component.css']
 })
 export class QuestResultsComponent implements OnInit {
+  public show: boolean = false;
+  public buttonName: any = "Results";
+
+  MESSAGE = "No questionnaires have been submitted yet!";
 
   results: FFQResultsResponse[] = [];
 
@@ -27,9 +31,18 @@ export class QuestResultsComponent implements OnInit {
       data.map(response => {
         this.results.push(response);
       });
-      console.log(this.results.length + ' foods and its nutrients were returned from server.');
+      console.log(
+        this.results.length +
+          " foods and its nutrients were returned from server."
+      );
       //this.dataLoaded = Promise.resolve(true);
     });
   }
-
+  toggle(index) {
+    this.results[index].show = !this.results[index].show;
+    // CHANGE THE NAME OF THE BUTTON.
+    if (this.results[index].show) this.buttonName = "Results";
+    else this.buttonName = "Results ";
+  }
+    
 }
