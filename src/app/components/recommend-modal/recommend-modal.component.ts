2d72d8013408421b1e64da5e8e75a9a4e0afa107
daykelms <daykelms@gmail.com>
2019-10-27 15:39:26 -0400

    recommendations component added and recommendations  modal added

---
+++ src/app/components/recommend-modal/recommend-modal.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+import {Router} from '@angular/router';
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+
+@Component({
+  selector: 'app-recommend-modal',
+  templateUrl: './recommend-modal.component.html',
+  styleUrls: ['./recommend-modal.component.css']
+})
+export class RecommendModalComponent {
+
+  @Input() id;
+ }
