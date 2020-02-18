a8b615480a0266b282c96fbce5479f92b4132ef6
unknown <nader796@gmail.com>
2020-02-18 17:07:22 -0500

    created parental portal header

---
+++ src/app/pages/parental-header/parental-header.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parental-header',
+  templateUrl: './parental-header.component.html',
+  styleUrls: ['./parental-header.component.css']
+})
+export class ParentalHeaderComponent {
+  TITLE = 'Parent Portal';
+
+}
--- src/app/pages/quest-id-input-page/quest-id-input.component.ts
+++ src/app/pages/quest-id-input-page/quest-id-input.component.ts
@@ -14,7 +14,7 @@ import {Observable} from 'rxjs';
   styleUrls: ['./quest-id-input.component.css']
 })
 export class QuestIdInputComponent {
-
+  TITLE = 'Parent Portal';
   questionnaire: QuestionnaireResponse;
 
   constructor(
