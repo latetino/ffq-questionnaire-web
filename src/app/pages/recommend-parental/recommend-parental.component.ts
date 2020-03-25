058bc0371478a656a49049e10933c70546dc7af4
unknown <nader796@gmail.com>
2020-03-24 21:04:41 -0400

    created cards for recommendation items

--- src/app/pages/recommend-parental/recommend-parental.component.ts
+++ src/app/pages/recommend-parental/recommend-parental.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecommendParentalComponent implements OnInit {
 
+  showBracketFirst = true;
+  showBracketSecond = false;
+  showBracketThird = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public showFirst() {
+    this.showBracketFirst = true;
+    this.showBracketSecond = false;
+    this.showBracketThird = false;
+  }
+  public showSecond() {
+    this.showBracketFirst = false;
+    this.showBracketSecond = true;
+    this.showBracketThird = false;
+  }
+  public showThird() {
+    this.showBracketFirst = false;
+    this.showBracketSecond = false;
+    this.showBracketThird = true;
   }
 
 }
