5f119c92df4eaca6919ad17393d647d1a7097aa6
unknown <nader796@gmail.com>
2020-03-31 21:56:31 -0400

    added tracker history page cards

---
+++ src/app/pages/tracker-history-page/tracker-history-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tracker-history-page',
+  templateUrl: './tracker-history-page.component.html',
+  styleUrls: ['./tracker-history-page.component.css']
+})
+export class TrackerHistoryPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
