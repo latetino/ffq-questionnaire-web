02c291135f469360e2a48c9b2cb1dc248f06a72b
unknown <nader796@gmail.com>
2020-03-31 21:53:27 -0400

    added tracker cards and styling

--- src/app/pages/tracker-page/tracker-page.component.ts
+++ src/app/pages/tracker-page/tracker-page.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrackerPageComponent implements OnInit {
 
+  TITLE = 'Nutrition Tracker';
+  MAIN_MESSAGE = 'Message';
+  INSTRUCTIONS_TITLE = 'Instructions: \n';
+  BULLETED_INSTRUCTIONS = [
+    'For each food item select the appropriate answer',
+    'If your child consumed the designated amount press the box',
+    'If your child consumed more than the designated amount press the up arrow',
+    'If your child consumed less than the designated amount press the down arrow',
+    'Click the submit button when finished.'
+  ];
+
+  showBracketFirst = true;
+  showBracketSecond = false;
+  showBracketThird = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public showFirst() {
+    this.showBracketFirst = true;
+    this.showBracketSecond = false;
+    this.showBracketThird = false;
+  }
+  public showSecond() {
+    this.showBracketFirst = false;
+    this.showBracketSecond = true;
+    this.showBracketThird = false;
+  }
+  public showThird() {
+    this.showBracketFirst = false;
+    this.showBracketSecond = false;
+    this.showBracketThird = true;
   }
 
 }
