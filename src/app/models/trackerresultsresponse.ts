2ca71e2f5379f2c6101203b2a860dc903170de24
unknown <nader796@gmail.com>
2020-04-10 18:34:40 -0400

    created tracker services and updated model

--- src/app/models/trackerresultsresponse.ts
+++ src/app/models/trackerresultsresponse.ts
@@ -1,10 +1,12 @@
 export class TrackerResultsResponse {
   userId: string;
+  age: number;
   date: string;
   responses: any;
 
-  constructor(userId: string, date: string, responses: any) {
+  constructor(userId: string, age: number, date: string, responses: any) {
     this.userId = userId;
+    this.age = age;
     this.date = date;
     this.responses = responses;
   }
---
+++ src/app/services/tracker-response/tracker-response.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TrackerResultsResponse } from 'src/app/models/trackerResultsResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TrackerResponseService {
+
+  endpoint = 'http://localhost:9090/ffq';
+
+  constructor(private http: HttpClient) { }
+
+  submitTracker(results: TrackerResultsResponse): Observable<any> {
+    return this.http.put(this.endpoint + '/tracker', results);
+  }
+
+}
