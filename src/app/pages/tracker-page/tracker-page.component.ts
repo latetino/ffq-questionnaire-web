06d5252821853116eff53943794947095e441453
unknown <nader796@gmail.com>
2020-04-10 18:35:56 -0400

    updated tracker page

--- src/app/pages/tracker-page/tracker-page.component.ts
+++ src/app/pages/tracker-page/tracker-page.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Description } from 'src/app/models/ffqfooddescription';
+import { Observable } from 'rxjs';
+import { FoodDescriptionService } from 'src/app/services/food-description/food-description.service';
+import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
+import { formatDate } from '@angular/common';
+import { ErrorDialogPopupComponent } from 'src/app/components/error-dialog-popup/error-dialog-popup.component';
+import { MatDialog } from '@angular/material';
+import { TrackerResultsResponse } from 'src/app/models/trackerResultsResponse';
+import { TrackerItems } from 'src/app/models/trackeritems';
+import { TrackerResponseService } from 'src/app/services/tracker-response/tracker-response.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-tracker-page',
@@ -12,35 +25,123 @@ export class TrackerPageComponent implements OnInit {
   INSTRUCTIONS_TITLE = 'Instructions: \n';
   BULLETED_INSTRUCTIONS = [
     'For each food item select the appropriate answer',
-    'If your child consumed the designated amount press the box',
+    'If your child consumed the designated amount press the checkbox',
     'If your child consumed more than the designated amount press the up arrow',
     'If your child consumed less than the designated amount press the down arrow',
     'Click the submit button when finished.'
   ];
 
-  showBracketFirst = true;
+  showBracketFirst = false;
   showBracketSecond = false;
   showBracketThird = false;
+  showItems = false;
+  showAgeForm = true;
+  age: number;
 
-  constructor() { }
+  foodResults: Description[];
+  trackerForm: FormGroup;
+  trackerResponse: TrackerResultsResponse;
+  trackerItems: TrackerItems[] = [];
+
+  constructor(public foodDescriptionService: FoodDescriptionService,
+              private formBuilder: FormBuilder,
+              private router: Router,
+              private authenticationService: AuthenticationService,
+              private submissionErrorDialog: MatDialog,
+              private successDialog: MatDialog,
+              private trackerResponseService: TrackerResponseService) {}
 
   ngOnInit() {
+    this.getAllResults();
+    //build form
+    this.trackerForm = this.formBuilder.group({
+      userId: [this.authenticationService.currentUserId],
+      responses: this.formBuilder.array([
+        this.formBuilder.group({
+          answer: [null, [Validators.required]]
+        })
+      ])
+    })
   }
 
-  public showFirst() {
-    this.showBracketFirst = true;
-    this.showBracketSecond = false;
-    this.showBracketThird = false;
-  }
-  public showSecond() {
-    this.showBracketFirst = false;
-    this.showBracketSecond = true;
-    this.showBracketThird = false;
-  }
-  public showThird() {
-    this.showBracketFirst = false;
-    this.showBracketSecond = false;
-    this.showBracketThird = true;
+  public submitTracker() {
+    let completed = true;
+    for(const response of this.trackerForm.controls.responses.value) {
+      if(!response.answer) {
+        completed = false;
+      }
+    }
+    if(completed) {
+      for(let i = 0; i < this.foodResults.length; i++) {
+        this.trackerItems.push(new TrackerItems(this.foodResults[i].foodItemGroupName,
+                                                this.trackerForm.controls.responses.value[i].answer))
+      }
+      this.trackerResponse = new TrackerResultsResponse(this.authenticationService.currentUserId,
+                                                        this.age,
+                                                        formatDate(new Date(), 'MM/dd/yyyy', 'en'),
+                                                        this.trackerItems);
+
+      this.trackerResponseService.submitTracker(this.trackerResponse).subscribe(() => {
+        const dialogRef = this.successDialog.open(ErrorDialogPopupComponent);
+        dialogRef.componentInstance.title = 'Submitted Successfully';
+        dialogRef.componentInstance.message = 'Your submission has been recorded.';
+        this.router.navigate(['/tracker-history']);
+      }, (error: HttpErrorResponse) => {
+        const  dialogRef  = this.submissionErrorDialog.open(ErrorDialogPopupComponent);
+        dialogRef.componentInstance.title = 'Submission Error';
+        dialogRef.componentInstance.message = error.message;
+        this.router.navigate(['/tracker-history']);
+      });
+      
+    } else {
+      const  dialogRef  = this.submissionErrorDialog.open(ErrorDialogPopupComponent);
+      dialogRef.componentInstance.title = 'Tracker Incomplete';
+      dialogRef.componentInstance.message = 'Please ensure all required fields are completed.';
+    }
   }
 
+  private getAllResults() {
+     const list: Observable<Description[]> = this.foodDescriptionService.getAllFoodItems();
+     list.subscribe(m => {
+       this.foodResults = m;
+       //create room in form for all items
+       for(let i = 1; i < this.foodResults.length; i++) {
+        this.addResponseRow();
+      }
+     });
+  }
+
+  private addResponseRow() {
+    const responsesArray = <FormArray>this.trackerForm.controls['responses'];
+    responsesArray.push(
+      this.formBuilder.group({
+        answer: [null, [Validators.required]]
+      }));
+  }
+
+  public enterAge(age: number) {
+    if(age) {
+      this.showAgeForm = false;
+      this.showItems = true;
+      this.age = age;
+      console.log(age);
+
+      if(age <= 12) {
+        this.showBracketFirst = false;
+        this.showBracketSecond = false;
+        this.showBracketThird = true;
+      }
+      if(age >= 6 && age < 12) {
+        this.showBracketFirst = false;
+        this.showBracketSecond = true;
+        this.showBracketThird = false;
+      }
+      else {
+        this.showBracketFirst = true;
+        this.showBracketSecond = false;
+        this.showBracketThird = false;
+      }
+    }
+  }
+  
 }
