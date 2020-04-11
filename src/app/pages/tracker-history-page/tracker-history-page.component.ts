560b39639a09629555086111e1220553cf065697
unknown <nader796@gmail.com>
2020-04-10 20:59:43 -0400

    updated tracker history to accept responses

--- src/app/pages/tracker-history-page/tracker-history-page.component.ts
+++ src/app/pages/tracker-history-page/tracker-history-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TrackerResultsResponse } from 'src/app/models/trackerResultsResponse';
+import { TrackerResultsService } from 'src/app/services/tracker-results/tracker-results.service';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
+import { TrackerItems } from 'src/app/models/trackeritems';
 
 @Component({
   selector: 'app-tracker-history-page',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrackerHistoryPageComponent implements OnInit {
 
-  constructor() { }
+  results: TrackerResultsResponse[] = [];
+  
+  constructor(private trackerResultsService: TrackerResultsService,
+              private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
+    this.getResultsByUser(this.authenticationService.currentUserId);
+    
+    // test items
+    // for(let i = 0; i < 10; i++) {
+    //   this.results[i] = new TrackerResultsResponse("1", i, "4/"+i+"/20", [new TrackerItems("food1", "Above"),
+    //                                                                       new TrackerItems("food2", "Equal"),
+    //                                                                       new TrackerItems("food3", "Below")]);
+    // }
   }
+
+  private getResultsByUser(userId: string) {
+    this.trackerResultsService.getResultsByUser(userId).subscribe(res => {
+      this.results = res.reverse();
+    });
+  };
 
 }
